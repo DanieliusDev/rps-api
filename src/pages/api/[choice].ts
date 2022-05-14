dd43df000220997f2be0e76850041dbfb24f7b8f
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-05-14 23:33:42 +0100

    Remove `random` choice

--- src/pages/api/[choice].ts
+++ src/pages/api/[choice].ts
@@ -1,40 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
-export interface APIResponse {
-	you: string;
-	computer: string;
-	winner: boolean | null;
-}
-
-interface APIError<Code extends keyof APIErrors = keyof APIErrors> {
-	code: Code;
-	message: APIErrors[Code];
-}
-
-interface APIErrors {
-	INVALID_CHOICE: 'The provided choice is invalid.';
-	METHOD_NOT_ALLOWED: string; 
-}
-
-const availableChoices = ['rock', 'paper', 'scissors'];
+import { APIError, APIResponse, availableChoices, genChoice, getWinner, resolveChoice } from '.';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<APIResponse | APIError>) {
-	if (req.method !== 'GET') res.status(405).json({ code: 'METHOD_NOT_ALLOWED', message: `Method ${req.method} is not allowed.` });
-
-	let { choice } = req.query as Record<string, string>;
-
-	if (choice === 'random') choice = availableChoices[Math.floor(Math.random() * availableChoices.length)];
-
-	if (!availableChoices.includes(choice) && choice !== 'random') return res.status(400).json({ code: 'INVALID_CHOICE', message: 'The provided choice is invalid.' });
-
-	const computer = availableChoices[Math.floor(Math.random() * availableChoices.length)];
-
-	let winner;
-	if (choice === computer) winner = null;
-	else if (choice === 'rock' && computer === 'scissors') winner = true;
-	else if (choice === 'paper' && computer === 'rock') winner = true;
-	else if (choice === 'scissors' && computer === 'paper') winner = true;
-	else winner = false;
-
-	res.status(200).json({ you: choice, computer, winner });
+	if (req.method !== 'GET')
+		res.status(405).json({ code: 'METHOD_NOT_ALLOWED', message: `Method ${req.method} is not allowed.` });
+	let { choice: player } = req.query as { choice: typeof availableChoices[number] };
+	if (!availableChoices.includes(player))
+		return res.status(400).json({ code: 'INVALID_CHOICE', message: 'The provided choice is invalid.' });
+	player = resolveChoice(player)!;
+	const computer = genChoice();
+	res.status(200).json({ you: player, computer, winner: getWinner(player, computer) });
 }
